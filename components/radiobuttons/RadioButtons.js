c9c40739fb0a347bb97576cf0f3eba10264165fa
Vitalii Adler <vitaliia@akdesign.ai>
2023-05-24 13:25:34 +0300

    some fixes

--- components/radiobuttons/RadioButtons.js
+++ components/radiobuttons/RadioButtons.js
@@ -8,37 +8,35 @@ const RadioButtons = () => {
     const params = useParams();
     const [value, setValue] = useState('');
     const onHandleChange = (event) => {
-        setValue(event.target.value);
         router.push(`/${params.city}/${event.target.value}`);
     }
 
     useEffect(() => {
         setValue(params.transit);
-    }, [])
+    }, [params])
 
     return (
         <div className='flex justify-evenly py-4'>
-            <div className="flex items-center mb-4">
+            <div className="flex items-center">
                 <input
                     checked={value === 'departures'}
-                    id="radio-1"
+                    id="deppartures"
                     type="radio"
                     value="departures"
-                    name="radio-button"
                     onChange={onHandleChange}
                 />
-                <label htmlFor="radio-2" className="ml-2">Departures</label>
+                <label htmlFor="deppartures" className="ml-2">Departures</label>
             </div>
             <div className="flex items-center">
                 <input
                     checked={value === 'arrivals'}
-                    id="radio-2"
+                    id="arrivals"
                     type="radio"
                     value="arrivals"
                     name="radio-button"
                     onChange={onHandleChange}
                 />
-                <label htmlFor="radio-2" className="ml-2">Arrivals</label>
+                <label htmlFor="arrivals" className="ml-2">Arrivals</label>
             </div>
         </div>
     );
